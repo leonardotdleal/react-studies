81fc871a92f8d699078b2ecd22bb61ecf33e4966
Leonardo Leal <leonardotade@gmail.com>
2019-09-04 08:22:55 -0300

    sample redux

--- react-counter-redux/src/CounterClass.jsx
+++ react-counter-redux/src/CounterClass.jsx
@@ -1,29 +1,37 @@
 /** rcrdux */
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import { DECREMENT, INCREMENT } from './redux/actions';
 
-import Button from './components/Button'
-import DivCounter from './components/DivCounter'
+import Button from './components/Button';
+import DivCounter from './components/DivCounter';
 
-const increment = () => ({ type: INCREMENT, origin: 'class' });
-const decrement = () => ({ type: DECREMENT, origin: 'class' });
+// const increment = () => ({ type: INCREMENT, origin: 'class' });
+// const decrement = () => ({ type: DECREMENT, origin: 'class' });
 
 class CounterClass extends Component {
   state = {
     countClass: 0
-  }
+  };
+
+  increment = () => {
+    this.props.dispatch({ type: INCREMENT, origin: 'class' });
+  };
+
+  decrement = () => {
+    this.props.dispatch({ type: DECREMENT, origin: 'class' });
+  };
 
   render() {
     return (
       <DivCounter>
         <h2>Counter with class</h2>
-        <Button action={this.props.decrement}>-</Button>
+        <Button action={this.decrement}>-</Button>
         <h3>{this.props.countClass}</h3>
-        <Button action={this.props.increment}>+</Button>
+        <Button action={this.increment}>+</Button>
       </DivCounter>
-    )
+    );
   }
 }
 
@@ -31,11 +39,16 @@ const mapStateToProps = state => ({
   countClass: state.countClass
 });
 
-const mapDispatchToProps = dispatch => {
-  return {
-    increment: () => dispatch(increment()),
-    decrement: () => dispatch(decrement()),
-  };
-}
+export default connect(mapStateToProps)(CounterClass);
 
-export default connect(mapStateToProps, mapDispatchToProps)(CounterClass);
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     increment: () => dispatch(increment()),
+//     decrement: () => dispatch(decrement())
+//   };
+// };
+
+// export default connect(
+//   mapStateToProps,
+//   mapDispatchToProps
+// )(CounterClass);
